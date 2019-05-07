e54710d33d0deaf8ce2d5f171b0af8513d50f2f5
Philipp Thuerwaechter <pithu@thuerwaechter.de>
2019-05-07 09:01:12 +0200

    Switch to tshark fields
    Make code more light weight, parsing line from exact fields instead of parsing complete json output.

--- compact-json-stream.js
+++ compact-json-stream.js
@@ -1,62 +1,53 @@
 #!/usr/bin/env node
 
-const JSONStream = require('JSONStream');
-const es = require('event-stream');
+const readline = require('readline');
 
-let packets = [];
+let packets = []
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
 
 function isLocalIp(ip) {
     return ip && ip.startsWith('192.168.2.');
 }
 
-function parseSrcPort(layers) {
-    if (layers.tcp) return layers.tcp['tcp.srcport'];
-    if (layers.udp) return layers.udp['udp.srcport'];
-}
-
-function parseDstPort(layers) {
-    if (layers.tcp) return layers.tcp['tcp.dstport'];
-    if (layers.udp) return layers.udp['udp.dstport'];
-}
-
-function parseBytes(layers) {
-    // if (layers.tcp) return parseInt(layers.tcp['tcp.len'], 10);
-    // if (layers.udp) return parseInt(layers.udp['udp.length'], 10);
-    return parseInt(layers.frame['frame.len'], 10);
-}
-
 function truncateProtocols(protocols) {
-    return protocols && protocols.split(':').slice(0, 8).join(':')
+    return protocols && protocols.split(':')[2]
 }
 
-process.stdin
-    .pipe(JSONStream.parse('*..layers'))
-    .pipe(es.mapSync(function (layers) {
-        if (!layers.ip) {
-            // console.error('unknown packet', layers);
-            return
-        }
-        const packet = {
-           frame_number: layers.frame['frame.number'],
-           frame_protocols: layers.frame['frame.protocols'],
-           frame_time_epoch: layers.frame['frame.time_epoch'],
-           bytes: parseBytes(layers),
-           ip_src: layers.ip['ip.src'],
-           ip_dst: layers.ip['ip.dst'],
-           ip_src_port: parseSrcPort(layers),
-           ip_dst_port: parseDstPort(layers),
-        };
-        packets.push(packet);
-    }));
+rl.on('line', function(line){
+    const [
+        frameLen,
+        frameProtocols,
+        ipSrc,
+        ipDst,
+        tcpSrcport,
+        tcpDstport,
+        udpSrcport,
+        udpDstport,
+    ] = line.split('\t');
+
+    packets.push({
+        frameLen: parseInt(frameLen, 10),
+        frameProtocols,
+        ipSrc,
+        ipDst,
+        ipSrcPort: tcpSrcport || udpSrcport,
+        ipDstPort: tcpDstport || udpDstport,
+    });
+});
 
 function compact() {
     const map = {};
     for (const packet of packets) {
-        const isUpload = isLocalIp(packet.ip_src);
-        const localIp = isUpload ? packet.ip_src : packet.ip_dst;
-        const remoteIp = isUpload ? packet.ip_dst : packet.ip_src;
-        const remotePort = isUpload ? packet.ip_dst_port : packet.ip_src_port;
-        const protocol = truncateProtocols(packet.frame_protocols);
+        const isUpload = isLocalIp(packet.ipSrc);
+        const localIp = isUpload ? packet.ipSrc : packet.ipDst;
+        const remoteIp = isUpload ? packet.ipDst : packet.ipSrc;
+        const remotePort = isUpload ? packet.ipDstPort : packet.ipSrcPort;
+        const protocol = packet.frameProtocols && packet.frameProtocols.split(':').slice(2, 8).join(':');
         const key = `${localIp}:${remoteIp}:${remotePort}:${protocol}`;
         if (!map[key]) {
             map[key] = {
@@ -70,9 +61,9 @@ function compact() {
         }
 
         if (isUpload) {
-            map[key].upload += packet.bytes;
+            map[key].upload += packet.frameLen;
         } else {
-            map[key].download += packet.bytes;
+            map[key].download += packet.frameLen;
         }
     }
     packets = [];

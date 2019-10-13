27db9bf7b5372219831f6f88d50373383379c172
Philipp Thuerwaechter <pithu@thuerwaechter.de>
2019-10-13 12:23:12 +0200

    append to capture file. do not override

--- run.sh
+++ run.sh
@@ -10,4 +10,4 @@ source ./fritz-capture.sh
 trap stopCapture SIGTERM
 
 # start capture
-startCapture | ./tshark-stream-add-timestamp.js - | ./tshark-stream-compact.js - > $WWW_ROOT/data/fritz-capture.csv
+startCapture | ./tshark-stream-add-timestamp.js - | ./tshark-stream-compact.js - >> $WWW_ROOT/data/fritz-capture.csv
